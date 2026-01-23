706e1856f4cd31ba09d691349611cdcdb2d57f72
nickyzinn16 <rickydavidbans27@gmail.com>
2026-01-23 18:38:58 -0100

    atualizacoes

--- js-testing/test.js
+++ js-testing/test.js
@@ -1,10 +1,22 @@
-let notaFinal = prompt("Insira a sua nota: ");
-console.log(notaFinal);
+// crie uma funcao que pede a altura e o peso da pessoa com pelomenos 5 casas decimais, 
+// realizar a operacao poara diminuir  numero de casa decimais para duas e depois calcular o imc
 
-if (notaFinal < 8) {
-    console.log("estas reprovado");
-} else if (notaFinal => 8 && notaFinal <= 10) {
-    console.log("vais para o exame");
-} else {
-    console.log("estas aprovado");
+function calcularIMC() {
+    let altura = prompt("Insira a altura:");
+    let peso = prompt("Insira o peso:");
+
+
+    let altura2 = altura;
+    altura2.toFixed(2);
+
+    let peso2 = peso;
+    peso2.toFixed(2);
+
+    imc = peso2 / (altura2 * altura2);
+    console.log(imc)
 }
+
+calcularIMC;
+
+    
+
