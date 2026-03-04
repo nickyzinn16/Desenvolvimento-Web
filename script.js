384df220a8dc030251d5b04a817969b42f158de5
nickyzinn16 <rickydavidbans27@gmail.com>
2026-03-04 18:46:19 -0100

    atualizacoes

---
+++ script.js
@@ -0,0 +1,25 @@
+// Selecao dos elementos
+const openBtn = document.querySelector("#openDialog");
+const closeBtn = document.querySelector("#closeDialog");
+const overlayArea = document.querySelector("#overlayArea");
+const sendBtn = document.querySelector("#sendBtn");
+
+// Funcoes da interacao
+function openDialog(){
+    overlayArea.classList.remove("hidden");
+}
+
+function closeDialog() {
+    overlayArea.classList.add("hidden");
+}
+
+openBtn.addEventListener("click", openDialog);
+
+closeBtn.addEventListener("click", closeDialog);
+
+
+overlayArea.addEventListener("click", function (event) {
+    if (event.target == overlayArea) {
+        closeDialog();
+    }
+})
